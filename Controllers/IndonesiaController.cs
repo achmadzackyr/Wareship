0f7def0d097f6e104e8562697e198c00f6816b6c
Achmad Zacky Rachmatullah <achmadzackyr@gmail.com>
2021-10-08 06:15:27 +0700

    Indonesia dropdown provinsi & city

---
+++ Controllers/IndonesiaController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RestSharp;
+using Wareship.ViewModel.Global;
+using Wareship.ViewModel.Indonesia;
+
+namespace Wareship.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IndonesiaController : ControllerBase
+    {
+        [HttpGet]
+        [Route("province")]
+        public IActionResult GetAllProvince()
+        {
+            var stat = new Status();
+            var resp = new ProvinceResponse();
+
+            var client = new RestClient("https://dev.farizdotid.com/api/daerahindonesia/provinsi");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            IRestResponse response = client.Execute(request);
+            if(response.IsSuccessful)
+            {
+                stat.ResponseCode = StatusCodes.Status200OK;
+                stat.ResponseMessage = "Success";
+                resp.Status = stat;
+                resp.Result = JsonConvert.DeserializeObject<ProvinceListResponse>(response.Content);
+            }
+            
+            return Ok(resp);
+        }
+
+        [HttpGet]
+        [Route("city/{id_provinsi}")]
+        public IActionResult GetAllCity(int id_provinsi)
+        {
+            var stat = new Status();
+            var resp = new CityResponse();
+
+            var client = new RestClient("https://dev.farizdotid.com/api/daerahindonesia/kota?id_provinsi=" + id_provinsi);
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            IRestResponse response = client.Execute(request);
+            if (response.IsSuccessful)
+            {
+                stat.ResponseCode = StatusCodes.Status200OK;
+                stat.ResponseMessage = "Success";
+                resp.Status = stat;
+                resp.Result = JsonConvert.DeserializeObject<CityListResponse>(response.Content);
+            }
+
+            return Ok(resp);
+        }
+    }
+
+
+}
---
+++ ViewModel/Indonesia/IndonesiaResponse.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Wareship.ViewModel.Global;
+
+namespace Wareship.ViewModel.Indonesia
+{
+    public partial class ProvinceResponse
+    {
+        public Status Status { get; set; }
+        public ProvinceListResponse Result { get; set; }
+    }
+
+    public partial class ProvinceListResponse
+    {
+        public List<Provinsi> Provinsi { get; set; }
+    }
+
+    public partial class Provinsi
+    {
+        public long Id { get; set; }
+        public string Nama { get; set; }
+    }
+
+
+    public partial class CityResponse
+    {
+        public Status Status { get; set; }
+        public CityListResponse Result { get; set; }
+    }
+
+    public partial class CityListResponse
+    {
+        public List<KotaKabupaten> Kota_Kabupaten { get; set; }
+    }
+
+    public partial class KotaKabupaten
+    {
+        public long Id { get; set; }
+        public long Id_Provinsi { get; set; }
+        public string Nama { get; set; }
+    }
+}
